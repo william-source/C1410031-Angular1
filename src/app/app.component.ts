b74babb100778630199fd32ed02b32bfba7b8095
william-source <c14180031@john.petra.ac.id>
2021-02-23 18:20:09 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-  player=1
-  baris=0;kolom=0
-  arr = Array[5][5]
-
-
-  onClick(){
-
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  player = 1;
+  baris = 0;
+  kolom = 0;
+  arr = Array[5][5];
+  brs: number;
+  klm: number;
+  onStart() {
+    for (this.brs = 0; this.brs < 5; this.brs++) {
+      for (this.klm = 0; this.klm < 5; this.klm++) {
+        this.arr[this.brs][this.klm] = "*";
+      }
+    }
   }
+
+  onClick() {}
 }
