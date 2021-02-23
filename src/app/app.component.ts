f1c5e0d46dd43bb86a6492cadd68f459cb4dd123
william-source <c14180031@john.petra.ac.id>
2021-02-23 18:15:28 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  player=1
+  baris=0;kolom=0
+  arr = Array[5][5]
+
+
+  onClick(){
+
+  }
 }
